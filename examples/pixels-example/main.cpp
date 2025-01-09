f21a108ed4af9c91bec185dcbc4c71fda6609b6e
whzruc <whzsxdt@126.com>
2025-01-09 15:41:58 +0000

    fix null vallue of string and pass clickbench single file tests

--- examples/pixels-example/main.cpp
+++ examples/pixels-example/main.cpp
@@ -7,8 +7,8 @@ using namespace duckdb;
 int main() {
 	DuckDB db(nullptr);
 	Connection con(db);
-//	std::string demo = ConfigFactory::Instance().getPixelsSourceDirectory() + "cpp/tests/data/example.pxl";
-    std::string demo = ConfigFactory::Instance().getPixelsSourceDirectory() + "cpp/cppout/1734406571.pxl";
+	std::string demo = ConfigFactory::Instance().getPixelsSourceDirectory() + "cpp/tests/data/example.pxl";
+//    std::string demo = ConfigFactory::Instance().getPixelsSourceDirectory() + "cpp/cppout/1734406571.pxl";
 	auto result = con.Query("SELECT * from '" + demo + "';");
 	result->Print();
 }
